0a5ff9226608202f7a6e8c31e01766a7e218eac4
Diofer Oviedo <dioferteonilo@gmail.com>
2023-01-29 11:39:44 -0300

    Java

---
+++ POO_J8/estoqueCourse/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product product = new Product();
+		System.out.println("Enter product data: ");
+		System.out.println("Name: ");
+		product.name = sc.nextLine();
+		System.out.println("Prince: ");
+		product.price = sc.nextDouble();
+		System.out.println("Quantity in stock: ");
+		product.quantity = sc.nextInt();		
+		
+		System.out.println(product.name + "," + product.price + "," + product.quantity);
+		
+		sc.close();
+		
+	}
+
+}
