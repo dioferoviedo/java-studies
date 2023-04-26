2408739e4bc330002573f1b37592c788bbecc800
Diofer Oviedo <dioferteonilo@gmail.com>
2023-04-26 14:27:49 -0300

    Java

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,13 @@
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+
+        int numero1 = 1;
+  
+        String numero2 = "2";
+  
+        System.out.println(numero1+numero2); 
+  
+      }
+
+}
+    
